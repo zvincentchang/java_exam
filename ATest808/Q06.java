c28374b425017f7c26a9ce0a7a6dc54d9afe0c80
VincHome <zvincentchang@gmail.com>
2018-11-30 16:38:19 +0800

    Add files via upload

---
+++ ATest808/Q06.java
@@ -0,0 +1,16 @@
+package ATest808;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+
+public class Q06 {
+  
+    public static void main(String[] args) {
+         LocalDateTime dt=LocalDateTime.of(2014,7,31,1,1);
+         dt.plusDays(30);
+         dt.plusMonths(1);
+         System.out.println(dt.format(DateTimeFormatter.ISO_DATE));
+    }
+
+}
